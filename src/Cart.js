932f7f476534129ec6719e58a410c72fc3c51850
guillenjs <jorgeguilln@gmail.com>
2020-09-05 19:13:55 -0400

    created cart containers and tabels

--- src/Cart.js
+++ src/Cart.js
@@ -4,7 +4,26 @@ export class Cart extends Component {
     render() {
         return (
             <div>
-                Cart
+
+                <div className="total">
+                    total
+                </div>
+
+                <div className="cart">
+                    <h1>cart</h1>
+                    <div className="table">
+                    <table>
+                        <tr>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                        </tr>
+                        <tr>
+                            <td>Salmon</td>
+                            <td>$20</td>
+                        </tr>
+                     </table>
+                    </div>
+                </div>
             </div>
         )
     }
